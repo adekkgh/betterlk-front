a93ab06c583a661b1f4988f6795e75ffc54742c5
GioevArsen <gioev.arsen15@list.ru>
2026-04-23 08:54:44 +0300

    added nav bar for mobile devices

---
+++ src/lib/components/mobileNav/MobileNav.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import './mobileNav.scss';
+	import { page } from '$app/stores';
+
+	const tabs = [
+		{
+			href: '/',
+			label: 'Главная',
+			icon: `<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>`,
+		},
+		{
+			href: '/homeworks',
+			label: 'Задания',
+			icon: `<path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"/><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"/>`,
+		},
+		{
+			href: '/journal',
+			label: 'Журнал',
+			icon: `<path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/>`,
+		},
+		{
+			href: '/profile',
+			label: 'Профиль',
+			icon: `<path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"/>`,
+		},
+	];
+
+	const isActive = (href: string) => {
+		if (href === '/') return $page.url.pathname === '/';
+		return $page.url.pathname.startsWith(href);
+	};
+</script>
+
+<nav class="mobile-nav">
+	{#each tabs as tab}
+		<a
+		href={tab.href}
+		class="mobile-nav__tab"
+		class:mobile-nav__tab--active={isActive(tab.href)}
+		>
+		<svg viewBox="0 0 20 20" fill="currentColor">
+			{@html tab.icon}
+		</svg>
+		<span>{tab.label}</span>
+		</a>
+	{/each}
+</nav>
